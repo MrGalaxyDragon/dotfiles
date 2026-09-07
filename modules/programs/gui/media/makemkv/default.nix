b7cb751952143bfec58108e74ea787a2d422d6e3
Tom Weelborg <135610355+tom-weelborg@users.noreply.github.com>
2026-09-07 17:07:10 +0200

    Update MakeMKV module to allow for settings configuration

--- modules/programs/gui/media/makemkv/default.nix
+++ modules/programs/gui/media/makemkv/default.nix
@@ -21,9 +21,37 @@
 
   userModule = {
     programs.gui.media.makemkv =
-      { }:
+      {
+        package ? null,
+        tokenPath ? null,
+        settingsDefault ? {
+          "app_DefaultSelectionString" = builtins.concatStringsSep "," [
+            "-sel:all"
+            "+sel:(favlang|nolang)"
+            "-sel:mvcvideo"
+            "=100:all"
+            "-10:favlang"
+            "+sel:attachment"
+          ];
+          "app_DestinationDir" = "";
+          "app_ExpertMode" = "1";
+          "app_Java" = "";
+          "app_Key" = "$key";
+          "app_Proxy" = "";
+          "app_ccextractor" = "";
+          "sdf_Stop" = "";
+        },
+        settingsOverride ? {}
+      }:
       { username }:
       { lib, pkgs, ... }:
+      let
+        settings = settingsDefault // settingsOverride;
+
+        settingsFileContent = lib.generators.toINIWithGlobalSection {} {
+          globalSection = builtins.mapAttrs (_: value: ''"${value}"'') settings;
+        };
+      in
       {
         boot.kernelModules = lib.mkAfter [
           "sg"
@@ -31,13 +59,57 @@
 
         users.users.${username} = {
           packages = lib.mkAfter [
-            pkgs.makemkv
+            (if (package != null) then package else pkgs.makemkv)
           ];
 
           extraGroups = lib.mkAfter [
             "cdrom"
             "plugdev"
             "video"
+          ];
+        };
+
+        systemd.services.makemkv-config = {
+          description = "Generate MakeMKV configuration";
+
+          wantedBy = [
+            "multi-user.target"
+          ];
+
+          before = [
+            "makemkv.service"
+          ];
+
+          serviceConfig = {
+            Type = "oneshot";
+            User = username;
+            UMask = "0077";
+          };
+
+          script = ''
+            set -euo pipefail
+
+            config_dir=/home/${username}/.MakeMKV/
+            config_file="$config_dir/settings.conf"
+
+            mkdir -p "$config_dir"
+
+            key=${lib.optionalString (tokenPath != null) "$(cat ${tokenPath})"}
+
+            cat > "$config_file" <<EOF
+            ${settingsFileContent}
+            EOF
+
+            chmod 600 "$config_file"
+          '';
+        };
+
+        systemd.services.makemkv = {
+          requires = [
+            "makemkv-config.service"
+          ];
+          after = [
+            "makemkv-config.service"
           ];
         };
       };
